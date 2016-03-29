fb9580ece61615945ffe6552f3cfe77c5744b5ad
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2016-03-29 15:50:59 -0600

    refactor scatterplot placement, remove some magic numbers

--- rugplot_pca.py
+++ rugplot_pca.py
@@ -6,7 +6,7 @@ from itertools import combinations
 
 from pprint import pprint
 
-pca = [list() for n in range(5)]
+
 
 colors = ['olivedrab', 'navajowhite',
           'indigo', 'tan',
@@ -21,41 +21,36 @@ for g in catreader:
     gen_color[g[0]] = colors[int(g[1])-1]
 
 
+
 reader = csv.reader(open('pca_targetspace.csv'))
-
 reader.next()
-
+pca = [list() for n in range(5)]
 markers = []
 for l in reader:
-    markers.append(CircleMarker(r=0.2, fill=gen_color[l[0]]))    
+    markers.append(CircleMarker(r=0.5, fill=gen_color[l[0]]))
     for n in range(1,6):
         pca[n-1].append(float(l[n]))
 
-    
 
 
-
-    
 
 npca = []
 for c in pca:
     tmp = np.array(c)
-    tmp *= 1.0/tmp.max()    
+    tmp *= 1.0/tmp.max()
     npca.append(tmp)
 
+frame_size = 200
+dash_size = 10
+jump = frame_size + dash_size
+
 rug = svgwrite.Drawing('example.svg')
-
 pairs = []
-for p in combinations(range(5),2):
-    pairs.append(p)
-
-for i in range(5):
-    for j in range(5):
-        if (i,j) in pairs:
-            s = Scatter(npca[i], npca[j], markers, insert=(i*200+10,j*200+10), size=(200,200))
-            s.drawBorder(stroke='grey', fill='white', stroke_width=0.8)
-            s.drawMarkers()
-            s.drawDotDash(['w','e','s','n'], dash_height=5, stroke="grey", stroke_width=0.2)
-            rug.add(s.dwg)
+for i,j in combinations(range(5), 2):
+    s = Scatter(npca[i], npca[j], markers, insert=(i*jump,j*jump), size=(frame_size, frame_size))
+    s.drawBorder(stroke='grey', fill='white', stroke_width=0.8)
+    s.drawMarkers()
+    s.drawDotDash(['w','e','s','n'], dash_height=5, stroke="grey", stroke_width=0.2)
+    rug.add(s.dwg)
 
 rug.save()
